96008117a6976bb66239de31c857fa1cffff2443
Shawn <smoreo22@aol.com>
2022-04-13 23:48:09 -0400

    added substitution.js file

--- src/substitution.js
+++ src/substitution.js
@@ -5,9 +5,84 @@
 
 const substitutionModule = (function () {
   // you can add any code you want within this function scope
-
+  const letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
   function substitution(input, alphabet, encode = true) {
     // your solution code here
+
+    if (alphabet === undefined || alphabet.length !== 26) {
+      return false;
+    }
+
+    let output = [];
+    let message = input.toLowerCase().split("");
+    let code = alphabet.split("");
+
+   
+    for (let i = 0; i < code.length; i++) {
+      let check = [];
+      for (let j = 0; j < code.length; j++) {
+        if (code[i] === code[j]) {
+          check.push(code[j]);
+          if (check.length > 1) {
+            return false;
+          }
+        }
+      }
+    }
+
+    if (encode === true) {
+      for (let i = 0; i < message.length; i++) {
+        if (message[i] === " ") {
+          output.push(" ");
+        }
+        for (let j = 0; j < letters.length; j++) {
+          if (message[i] === letters[j]) {
+            output.push(code[j]);
+          }
+        }
+      }
+    }
+    if (encode === false) {
+      for (let i = 0; i < message.length; i++) {
+        if (message[i] === " ") {
+          output.push(" ");
+        }
+        for (let j = 0; j < code.length; j++) {
+          if (message[i] === code[j]) {
+            output.push(letters[j]);
+          }
+        }
+      }
+    }
+
+    return output.join("");
   }
 
   return {
--- test/substitution.test.js
+++ test/substitution.test.js
@@ -1,1 +1,13 @@
 // Write your tests here!
+    // error handling
+    //   1) should return false if the substitution alphabet is missing
+    //   2) should return false if the substitution alphabet is not exactly 26 characters
+    //   3) should return false if the substitution alphabet does not contain unique characters
+    // encoding a message
+    //   4) should encode a message by using the given substitution alphabet
+    //   5) should work with any kind of key with unique characters
+    //   6) should preserve spaces
+    // decoding a message
+    //   7) should decode a message by using the given substitution alphabet
+    //   8) should work with any kind of key with unique characters
+    //   9) should preserve spaces
